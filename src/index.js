15d78020cd6d4d8f94c60640ae801c79ccf3437c
furkancoram <furkancoram@gmail.com>
2025-04-08 21:54:58 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ChatBot from './ChatBot';
+import EvciAI from './ChatBot';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChatBot />
+    <EvciAI />
   </React.StrictMode>
 );
