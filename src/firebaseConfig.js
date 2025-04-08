720c7da2e69301e825e0c6cc8ee75d34dff633b8
furkancoram <furkancoram@gmail.com>
2025-04-09 00:04:08 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,5 @@
 const firebaseConfig = {
- apiKey: "AIzaSyAy~75uUn5dTPoTzSGar1NHRKy9rEzMDFU",
+  apiKey: "AIzaSyAy~75uUn5dTPoTzSGar1NHRKy9rEzMDFU",
   authDomain: "evciai.firebaseapp.com",
   projectId: "evciai",
   storageBucket: "evciai.appspot.com",
@@ -7,3 +7,7 @@ const firebaseConfig = {
   appId: "1:263153257523:web:474c43a65342c3d3157eec",
   measurementId: "G-MZL1HE09MY"
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
