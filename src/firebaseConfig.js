aeed3a18436554a96f965c157bb19e12d61d5586
furkancoram <furkancoram@gmail.com>
2025-04-08 23:01:42 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWEg9wod_9ZeQtaMVsj6SF4RfA0_UKACg",
-  authDomain: "evcia1.firebaseapp.com",
-  projectId: "evcia1",
-  storageBucket: "evcia1.appspot.com",
-  messagingSenderId: "604168969295",
-  appId: "1:604168969295:web:d57feb6fe69b418e3085e2",
-  measurementId: "G-6PWNMNKLDZ"
+  apiKey: "AIzaSyCWEq9wod_9ZeQtaMVsj6SF4RfAO_UKACg",
+  authDomain: "evciai.firebaseapp.com",
+  projectId: "evciai",
+  storageBucket: "evciai.appspot.com",
+  messagingSenderId: "604168996295",
+  appId: "1:604168996295:web:d57feb6fe69b418e3085e2",
+  measurementId: "G-6PWENNKLDZ"
 };
 
 const app = initializeApp(firebaseConfig);
